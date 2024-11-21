221aa258df80129885628d05b48b9cd0922931d7
Ekapurnama06 <purnamae825@gmail.com>
2024-11-21 12:41:30 +0700

    toko buku onile2

--- toko-buku-online/app/Config/Routes.php
+++ toko-buku-online/app/Config/Routes.php
@@ -1,8 +1,10 @@
 <?php
-
 use CodeIgniter\Router\RouteCollection;
-
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('chart', 'Home::chart');
+$routes->get('checkout', 'Home::checkout');
+$routes->get('search', 'Home::search');
+$routes->post('submit', 'Home::submit');
