34efc563388a3dda6067448f78bc891483bc9894
Ekapurnama06 <purnamae825@gmail.com>
2024-12-20 11:01:45 +0700

    Toko Buku Online - Admin Data Buku

--- toko-buku-online/app/Config/Routes.php
+++ toko-buku-online/app/Config/Routes.php
@@ -1,5 +1,7 @@
 <?php
+
 use CodeIgniter\Router\RouteCollection;
+
 /**
  * @var RouteCollection $routes
  */
@@ -10,6 +12,7 @@ $routes->get('checkout', 'Home::checkout');
 $routes->get('search', 'Home::search');
 service('auth')->routes($routes);
 $routes->post('submit', 'Home::submit');
+$routes->get('images/(:segment)', 'Home::images/$1');
 service('auth')->routes($routes);
 
 
@@ -20,4 +23,7 @@ $routes->group('admin', ['filter' => 'group:admin'], function ($routes) {
   $routes->get('databuku', 'AdminController::databuku');
   $routes->get('datatransaksi', 'AdminController::datatransaksi');
   $routes->get('datapelanggan', 'AdminController::datapelanggan');
+  $routes->post('databuku', 'AdminController::create_buku');
+  $routes->get('databuku/(:segment)/edit', 'AdminController::edit_buku/$1');
+  $routes->post('databuku/(:segment)/update', 'AdminController::update_buku/$1');
 });
--- toko-buku-online/app/Database/Migrations/2024-12-20-033355_CreateBukuTable.php
+++ toko-buku-online/app/Database/Migrations/2024-12-20-033355_CreateBukuTable.php
@@ -11,43 +11,37 @@ class CreateBukuTable extends Migration
         $this->forge->addField([
             'id' => [
                 'type'           => 'INT',
-                'constraint'     => 11,
+                'constraint'     => 5,
                 'unsigned'       => true,
                 'auto_increment' => true,
             ],
             'judul' => [
-                'type'           => 'VARCHAR',
-                'constraint'     => 250,
-
-
+                'type'       => 'VARCHAR',
+                'constraint' => 250,
             ],
             'pengarang' => [
-                'type'           => 'VARCHAR',
-                'constraint'     => 255,
-
+                'type'       => 'VARCHAR',
+                'constraint' => 250,
             ],
             'penerbit' => [
-                'type'           => 'VARCHAR',
-                'constraint'     => 250,
-
+                'type'       => 'VARCHAR',
+                'constraint' => 250,
             ],
             'tahun' => [
-                'type'           => 'INT',
-                'constraint'     => 5,
+                'type'       => 'INT',
+                'constraint' => 5
             ],
             'thumbnail_url' => [
-                'type'           => 'VARCHAR',
-                'constraint'     => 255,
-
-            ],
+                'type'       => 'VARCHAR',
+                'constraint' => 250
+            ]
         ]);
-
         $this->forge->addKey('id', true);
         $this->forge->createTable('books');
     }
 
     public function down()
     {
-        $this->forge->dropTable('books');
+        $this->forge->dropTable('books', true);
     }
 }
